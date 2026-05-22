5b77d559073810a3ae7114ca833ae1ce656f8838
ymy0107 <2168526598@qq.com>
2026-05-22 22:24:20 +0800

    new

--- hysteria/config.sh
+++ hysteria/config.sh
@@ -1,6 +1,11 @@
 # bash <(curl -fsSL https://get.hy2.io/)
 
+# mkdir -p /etc/hysteria/certs
+# cp /etc/letsencrypt/live/verygood.us.kg/fullchain.pem /etc/hysteria/certs/
+# cp /etc/letsencrypt/live/verygood.us.kg/privkey.pem /etc/hysteria/certs/
+# chown -R hysteria:hysteria /etc/hysteria/certs
+# chmod 600 /etc/hysteria/certs/*
 cp /root/proxy-config/hysteria/config.yaml /etc/hysteria/config.yaml
 systemctl enable hysteria-server
-systemctl start hysteria-server
+systemctl restart hysteria-server
 systemctl status hysteria-server --no-pager
