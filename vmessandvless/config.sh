e521c0cebb9f1a4a0854b9e7413dd90026757245
ymy0107 <2168526598@qq.com>
2026-05-22 19:39:35 +0800

    Update config.sh

--- vmessandvless/config.sh
+++ vmessandvless/config.sh
@@ -1,4 +1,5 @@
 cp /root/proxy-config/vmessandvless/v2ray-linux-64/v2ray /usr/local/bin/v2ray
+mkdir -p /usr/local/etc/v2ray/
 cp /root/proxy-config/vmessandvless/v2ray-linux-64/config.json /usr/local/etc/v2ray/config.json
 cp /root/proxy-config/vmessandvless/v2ray-linux-64/systemd/system/v2ray.service /etc/systemd/system/v2ray.service
 
