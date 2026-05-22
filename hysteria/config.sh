b26fd8f4b11ece9cf410b1fc137c71770ced7120
ymy0107 <2168526598@qq.com>
2026-05-22 22:07:51 +0800

    Update config.sh

--- hysteria/config.sh
+++ hysteria/config.sh
@@ -1,6 +1,6 @@
 # bash <(curl -fsSL https://get.hy2.io/)
 
 cp /root/proxy-config/hysteria/config.yaml /etc/hysteria/config.yaml
-systemctl enable hysteria
-systemctl restart hysteria
-systemctl status hysteria --no-pager
+systemctl enable hysteria-server
+systemctl start hysteria-server
+systemctl status hysteria-server --no-pager
