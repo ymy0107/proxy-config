6c726b9dfcf57867cb45564c678e1a5752e51631
ymy0107 <2168526598@qq.com>
2026-05-24 17:56:26 +0800

    new

--- tuic/config.sh
+++ tuic/config.sh
@@ -4,3 +4,4 @@ cp /root/proxy-config/mihomo/mihomo.service /etc/systemd/system/mihomo.service
 systemctl daemon-reload
 systemctl enable mihomo.service
 systemctl restart mihomo.service
+systemctl status mihomo.service --no-pager
