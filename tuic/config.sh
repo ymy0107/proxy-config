1aa89750bde3ca540814c9594763fbb5f8a8f04b
ymy0107 <2168526598@qq.com>
2026-05-24 18:12:54 +0800

    Update config.sh

--- tuic/config.sh
+++ tuic/config.sh
@@ -1,5 +1,6 @@
 cp /etc/letsencrypt/live/verygood.us.kg/fullchain.pem /root/.config/mihomo/fullchain.pem
 cp /etc/letsencrypt/live/verygood.us.kg/privkey.pem /root/.config/mihomo/privkey.pem
+cp /root/proxy-config/tuic/config.yaml /root/.config/mihomo/config.yaml
 cp /root/proxy-config/mihomo/mihomo.service /etc/systemd/system/mihomo.service
 systemctl daemon-reload
 systemctl enable mihomo.service
