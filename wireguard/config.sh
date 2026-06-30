71b9afd937aad4eea6fc6518554d3054c10563fc
ymy0107 <2168526598@qq.com>
2026-06-30 15:01:16 +0800

    new

--- wireguard/config.sh
+++ wireguard/config.sh
@@ -1,6 +1,7 @@
 apt update
 apt install wireguard iptables -y
+echo "" > /etc/wireguard/wg0.conf
 cp /root/proxy-config/wireguard/wg0.conf /etc/wireguard/wg0.conf
-systemctl start wg-quick@wg0
+systemctl restart wg-quick@wg0
 systemctl enable wg-quick@wg0
 systemctl status wg-quick@wg0 --no-pager
