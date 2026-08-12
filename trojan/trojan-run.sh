d32b3ea842e38442d16176cc8fef7148285505fb
ymy0107 <2168526598@qq.com>
2026-08-12 17:38:46 +0800

    new

--- trojan/trojan-run.sh
+++ trojan/trojan-run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-pkill -f "trojan"
-stdbuf -oL -eL /root/proxy-config/trojan/trojan -c /root/proxy-config/trojan/config.json >> /root/proxy-config/trojan/trojan.log 2>&1 & true
+# pkill -f "trojan"
+# stdbuf -oL -eL /root/proxy-config/trojan/trojan -c /root/proxy-config/trojan/config.json >> /root/proxy-config/trojan/trojan.log 2>&1 & true
+cp /root/proxy-config/trojan/trojan /usr/bin/trojan
+cp /root/proxy-config/trojan/config.json /etc/trojan/config.json
+cp /root/proxy-config/trojan/trojan.service /etc/systemd/system/trojan.service
+
+systemctl daemon-reload
+systemctl enable trojan.service
+systemctl restart trojan.service
+systemctl status trojan.service --no-pager
