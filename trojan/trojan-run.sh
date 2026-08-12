02e612780bde6ce86b56d43659c6278933f89678
ymy0107 <2168526598@qq.com>
2026-08-12 17:39:50 +0800

    Update trojan-run.sh

--- trojan/trojan-run.sh
+++ trojan/trojan-run.sh
@@ -2,6 +2,7 @@
 # pkill -f "trojan"
 # stdbuf -oL -eL /root/proxy-config/trojan/trojan -c /root/proxy-config/trojan/config.json >> /root/proxy-config/trojan/trojan.log 2>&1 & true
 cp /root/proxy-config/trojan/trojan /usr/bin/trojan
+mkdir -p /etc/trojan
 cp /root/proxy-config/trojan/config.json /etc/trojan/config.json
 cp /root/proxy-config/trojan/trojan.service /etc/systemd/system/trojan.service
 
